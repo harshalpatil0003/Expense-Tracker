7de9cca548f485a468d6233408aaf1abb606f859
Phobial <harshalpatilcsn2020@gmail.com>
2024-08-13 17:07:37 +0530

    Routing is implemented

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,22 @@
-import react from 'react' 
+
 import ReactDom from 'react-dom/client'
 import { RouterProvider,createBrowserRouter } from "react-router-dom";
+import Home from './views/Home/Home';
+import Login from './views/Login/Login';
+import Signup from './views/Signup/Signup';
 
 const root=ReactDom.createRoot(document.getElementById('root'))
 const router=createBrowserRouter([{
     path: '/',
-    element: <Home />
+    element: <Home/>
+},
+{
+    path: '/login',
+    element:<Login/>
+},
+{
+    path:'/signup',
+    element:<Signup/>
 },
 {
     path: '*',
