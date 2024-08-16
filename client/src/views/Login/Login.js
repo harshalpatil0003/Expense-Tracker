ef68cbe6029413b1dc3494ab9b209a5b858db8c2
Phobial <harshalpatilcsn2020@gmail.com>
2024-08-16 11:51:10 +0530

    login and signup apis are merged in form

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -3,19 +3,23 @@ import { Link } from 'react-router-dom'
 import '../Signup/Signup.css'
 
 function Login() {
+  const login=async()=>{
+    const response=await axios.post(`${process.env.REAT_APP_API_URL}/v1/signins`)
+
+  }
   return (
     <div>
       <h1 className='text-center m-4'>User Login</h1>
       <form className=' input-form  d-block mx-auto p-4 rounded-3 my-5'>
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control " id="exampleInputEmail1" aria-describedby="emailHelp" required placeholder='Email'/>
+                    <input type="email" className="form-control " id="exampleInputEmail1" aria-describedby="emailHelp"  placeholder='Email'/>
                 </div>
                 <div class="mb-3">
-                    <input type="password" className="form-control" id="exampleInputPassword1" required placeholder='Password'/>
+                    <input type="password" className="form-control" id="exampleInputPassword1"  placeholder='Password'/>
                 </div>
 
-                <button type="submit" className="btn btn-success border border-0 entry-btn d-block mx-auto form-control">Rgister Now</button>
+                <button type="submit" className="btn btn-success border border-0 entry-btn d-block mx-auto form-control"> < Link to="/">Signin</Link></button>
                 <p className='text-center my-3'>Don't have an account <Link to='/signup'>SignUp</Link> 
                 </p>
             </form>
--- client/src/views/Signup/Signup.js
+++ client/src/views/Signup/Signup.js
@@ -1,29 +1,58 @@
 import React from 'react'
+import axios from 'axios'
+import {Toaster,toast} from 'react-hot-toast'
+import { useState } from 'react'
 import './Signup.css'
 import {Link} from 'react-router-dom'
 
 function Signup() {
+    const [user ,setUser]=useState({
+        name:'',
+        email:'',
+        password:''
+    })
+    const signup= async()=>{
+        const response=await axios.post(`${process.env.REAT_APP_API_URL}/v1/signups`,{
+            name:user.name,
+            email:user.email,
+            password:user.password
+
+        })
+        if(response.data.success){
+            toast.success(response.data.message)
+        }
+        else{
+            toast.error(response.data.message)
+
+        }
+    }
     return (
         <div>
             <h1 style={{textAlign:'center',margin:'20px'}}>User Registration</h1>
             <form className=' input-form  d-block mx-auto p-4 rounded-3 my-5'>
             <div className="mb-3">
-                    <input type="text" className="form-control " id="exampleInputEmail1" aria-describedby="emailHelp" required  placeholder='Name'/>
+                    <input type="text" className="form-control " id="exampleInputEmail1" aria-describedby="emailHelp"   placeholder='Name' value={user.name} 
+                    onChange={(e)=>setUser({...user,name:e.target.value})} />
+
                 </div>
                 <div className="mb-3">
-                    <input type="email" className="form-control " id="exampleInputEmail1" aria-describedby="emailHelp" required  placeholder='Email'/>
+                    <input type="email" className="form-control " id="exampleInputEmail1" aria-describedby="emailHelp"   placeholder='Email' value={user.email}
+                    onChange={(e)=>setUser({...user,email:e.target.value})} />
+                    
                 </div>
                 <div class="mb-3">
-                    <input type="password" className="form-control" id="exampleInputPassword1" required placeholder='password'/>
+                    <input type="password" className="form-control" id="exampleInputPassword1" required placeholder='password' value={user.password}
+                       onChange={(e)=>setUser({...user,password:e.target.value})}/>
                 </div>
                 <div class="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input " id="exampleCheck1" required />
+                    <input type="checkbox" className="form-check-input " id="exampleCheck1"  />
                     <label className="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
-                <button type="submit" className="btn btn-success border border-0 entry-btn d-block mx-auto form-control">Rgister Now</button>
+                <button type="submit" className="btn btn-success border border-0 entry-btn d-block mx-auto form-control" onClick={signup}>Rgister Now</button>
                 <p className='text-center my-3'>Already have an account <Link to='/login'>Login</Link> 
                 </p>
             </form>
+            <Toaster/>
         </div>
     )
 }
